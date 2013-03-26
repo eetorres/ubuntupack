bf1f050f5e1cb59003a2970ce7d178a06d36fafd
eetorres <eetorres@gmail.com>
2013-03-26 09:31:16 -0600

    Adding initial files
     Committer: eetorres <eetorres@gmail.com>
    
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   must-install.sh
    	new file:   tweaks.sh
    	new file:   workstation.sh

---
+++ must-install.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# Copyrigth 2007-2013 by Edmanuel Torres, eetorres@gmail.com
+#
+# Don't hesitate to contact me for any question, suggestion,
+# modification or bug. Get the latest version at:
+# http:#msmvtl.sourceforge.net
+#
+# This library is free  software;  you  can  redistribute  it and/or
+# modify it  under  the  terms  of  the   GNU Library General Public
+# License  as  published  by  the  Free  Software Foundation; either
+# version 2 of the License,  or  (at your option)  any later version.
+#
+# This  library  is  distributed  in the hope that it will be useful,
+# but  WITHOUT ANY WARRANTY;  without  even  the  implied warranty of
+# MERCHANTABILITY  or FITNESS FOR A PARTICULAR PURPOSE.   See the GNU
+# Library General Public License for more details.
+#
+# You should have  received a copy  of the GNU Library General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA.
+
+# Update Repositories
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update
+
+#echo "Instaled linux kernels:"
+#dpkg -l linux-*
+#dpkg -l linux-* | awk '/^ii/{ print $2 }'
+
+#echo "Actual kernel:"
+#uname -r | cut -f1,2 -d"-"
+
+# echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf; sudo apt-get -y update
+
+echo "================Intalling Software================"
+
+# BackInTime
+sudo apt-get -y install backintime-gnome
+
+# Texmaker
+sudo apt-get -y install texmaker
+
+# kile 
+sudo apt-get -y install kile
+
+# Chromium
+sudo apt-get -y install chromium-browser
+
+# XFig
+sudo apt-get -y install xfig
+
+# VLC
+sudo apt-get -y install vlc
+
+# DjView
+sudo apt-get -y install djview
+
+# Gimp
+sudo apt-get -y install gimp
+
+# Gmount-iso
+sudo apt-get -y install gmountiso
+
+# Desktop recorder
+sudo apt-get -y install gtk-recordmydesktop
+
+# gFTP
+sudo apt-get -y install gftp
+
+# GNOME Do
+sudo apt-get -y install gnome-do
+
+# Transmission
+sudo apt-get -y install transmission
+
+# WinFF
+sudo apt-get -y install winff
+
+# DropBox
+sudo apt-get -y install nautilus-dropbox
+
+# Meld Diff Viewer
+sudo apt-get -y install meld
+
+# Avogadro
+sudo apt-get -y install avogadro
+
+# Scilab
+sudo apt-get -y install scilab
+
+# Simple Scan
+sudo apt-get -y install simple-scan
+
+# XSane
+sudo apt-get -y install xsane
+sudo apt-get -y install gscan2pdf
+sudo apt-get -y install gv gocr
+
+# Okular
+sudo apt-get -y install okular
+
+# Shutter
+sudo apt-get -y install shutter
+
+# GNOME Tweak Tool
+sudo apt-get -y install gnome-tweak-tool
+
+# Gtk-WARNING **: Unable to locate theme engine in module_path: "pixmap",
+sudo apt-get -y install gtk2-engines-pixbuf
+
+# GConf-Editor
+sudo apt-get -y install gconf-editor
+
+# Install Ubuntu Restricted Extras
+sudo apt-get -y install ubuntu-restricted-extras
+
+# Ejecter: to easily and safely unmount external devices
+sudo apt-get -y install ejecter
+
+#### Unity Utils ####
+
+# Unity utilities lens
+sudo apt-get -y install unity-lens-utilities
+
+# xfig fonts
+sudo apt-get -y install gsfonts-x11
+
+echo "================Done================"
---
+++ workstation.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Copyrigth 2007-2013 by Edmanuel Torres, eetorres@gmail.com
+#
+# Don't hesitate to contact me for any question, suggestion,
+# modification or bug. Get the latest version at:
+# http:#msmvtl.sourceforge.net
+#
+# This library is free  software;  you  can  redistribute  it and/or
+# modify it  under  the  terms  of  the   GNU Library General Public
+# License  as  published  by  the  Free  Software Foundation; either
+# version 2 of the License,  or  (at your option)  any later version.
+#
+# This  library  is  distributed  in the hope that it will be useful,
+# but  WITHOUT ANY WARRANTY;  without  even  the  implied warranty of
+# MERCHANTABILITY  or FITNESS FOR A PARTICULAR PURPOSE.   See the GNU
+# Library General Public License for more details.
+#
+# You should have  received a copy  of the GNU Library General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA.
+
+# Update Repositories
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update
+
+#Install utils:
+sudo apt-get -y install mc
+sudo apt-get -y install subversion
+sudo apt-get -y install cvs git-core mercurial
+sudo apt-get -y install vim
+sudo apt-get -y install htop
+sudo apt-get -y install tree
+sudo apt-get -y install openssh-server
+
+# mpiexec, mpiexec.openmpi, mpirun, mpirun.openmpi,
+# ompi-clean, ompi-iof, ompi-ps, ompi-server, ompi_info,
+# orte-clean, orte-iof, orte-ps, orted, orterun
+sudo apt-get -y install openmpi-bin
+
+
+#Install C and C++ Compilers in Ubuntu
+sudo apt-get -y install build-essential
+sudo apt-get -y install build-essential checkinstall
+
+#Fortran
+sudo apt-get -y install gfortran
+
+#Autotools:
+sudo apt-get -y install autoconf automake autotools-dev m4
+
+#X11:
+sudo apt-get -y install libx11-dev libxrandr-dev
+
+#OpenGL:
+sudo apt-get -y install freeglut3-dev
+
+#Image libraries
+sudo apt-get -y install libjpeg-dev libpng-dev libz-dev
+
+#FreeType-based font drawing library for X (development files)
+sudo apt-get -y install libxft-dev
+
+#Xft provides a client-side font API for X applications, making the #FreeType font rasterizer available to X clients. Fontconfig is used #for font specification resolution. Where available, the RENDER #extension handles glyph drawing; otherwise, the core X protocol is #used. This package provides a static library and C header files. 
+
+#Install Xinerama
+sudo apt-get -y install libxinerama-dev
+
+#Image Libraries: JPEG=System
+#                 PNG=System
+#                 ZLIB=System
+
+#==================================
+# compile FLTK for Linux x86
+#libc6-dev-i386, lib32z1-dev, g++-multilib
+sudo apt-get -y install g++-multilib libc6-dev-i386 lib32z1-dev
+# ./configure --with-archflags=-m32
+
+#==================================
+# compile FLTK for win32
+sudo apt-get -y install mingw-w64
+sudo apt-get -y install wine
+# ./configure --host=i686-w64-mingw32 --build=x86_64-linux-gnu --prefix=/usr/i686-w64-mingw32
+
+# ftgl-2.1.3~rc5-win32 $ ./configure --prefix=/usr --host=i686-w64-mingw32 --build=x86_64-linux-gnu  --with-gl-inc="/usr/i686-w64-mingw32/include" --with-gl-lib="-L/usr/i686-w64-mingw32/lib -lglu32 -lopengl32 -lole32 -luuid -lcomctl32 -mwindows" CXXFLAGS="-I/usr/include"
+
+
+# $ gcc -print-multi-os-directory -m32
+# $ gcc -print-multi-os-directory -m64
+# $ gcc -print-multi-os-directory
+# $ gcc -print-multi-lib
+
+#sudo apt-get -y --fix-missing
+
+
+
+
